0a26ca8cc170093461b0487f8811b7439da9eec2
Kenr0t <kenrot.shooter@gmail.com>
2018-04-10 12:20:41 +0200

    Clase 10/04/18

---
+++ 10-04/comprime.pl
@@ -0,0 +1,8 @@
+﻿% comprime(+L, -R)
+% es cierto cuando R unifica con una lista de esta forma %
+% comprime([a, a, a, b, b, c]); R=[(a, 3), (b, 2), (c, 1)].
+
+comprime([], []).
+comprime([Ca], [(Ca,1)]).
+comprime([Ca, Ca|Resto], [(Ca,N1)| R]):- comprime([Ca|Resto], [(Ca,N2)|R]), N1 is N2+1.
+comprime([Ca, Cb|Resto], [(Ca,1)|R]):- Ca\=Cb, comprime([Cb|Resto], R).
---
+++ 10-04/descomprime.pl
@@ -0,0 +1,15 @@
+﻿% descomprime(-L, +R)
+% es cierto cuando R unifica con una lista de esta forma %
+% R=[(a, 3), (b, 2), (c, 1)]. descomprime([a, a, a, b, b, c])
+
+% CORREGIR
+descomprime([], []).
+descomprime([Ca], [(Ca,1)]).
+descomprime([Ca, Ca|Resto], [(Ca,N1)| R]):- descomprime([Ca|Resto], [(Ca,N2)|R]), N1 is N2+1.
+descomprime([Ca|Lista], [(Ca,1)|R]):- descomprime(Lista, R).
+
+
+% descomprime([], []).
+% descomprime([Ca], [(Ca,1)]).
+% descomprime([Ca, Ca|Resto], [(Ca,N1)| R]):- descomprime([Ca|Resto], [(Ca,N2)|R]), N1 is N2+1.
+% descomprime([Ca, Cb|Resto], R):- descomprime([Cb|Resto], [(Ca,1)|R]).
